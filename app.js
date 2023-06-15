3c9a63c0fd08d493e884a311fec0779a9b2f87ad
Nayan Dey <nayande.work@gmail.com>
2023-06-15 08:03:13 +0530

    version 4
    you can now remove items from the ui and also from the local storage

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const clear = document.querySelector(".clear-btn");
 const filter = document.querySelector(".filter");
 const filterInput = document.querySelector(".filter-input");
 const main = document.querySelector("main");
+//adding item from input box value
 function addItem(e) {
   e.preventDefault();
   const inputValue = inputBox.value;
@@ -14,34 +15,78 @@ function addItem(e) {
     alert("Write something first");
     return;
   }
-  const li = document.createElement("li");
-  li.className = "items";
-  const btn = createButton("btn delete");
-  li.appendChild(document.createTextNode(inputValue));
-  btn.appendChild(document.createTextNode("Delete"));
-  li.appendChild(btn);
-  listItems.appendChild(li);
-
-  // mainDiv.innerHTML = `<div class="items">${inputValue}</div><button class="btn delete">Delete</button>`;
-  // itemList.appendChild(mainDiv);
+  addItemsToDOM(inputValue);
+  addItemsToStorage(inputValue);
   inputBox.value = "";
   clearAllHide();
 }
+//creating a button which can add class
 function createButton(classes) {
   const btn = document.createElement("button");
   btn.className = classes;
   return btn;
 }
-function removeItem(e) {
+//add Items to the DOM
+function addItemsToDOM(item) {
+  const li = document.createElement("li");
+  li.className = "items";
+  const btn = createButton("btn delete");
+  li.appendChild(document.createTextNode(item));
+  btn.appendChild(document.createTextNode("Delete"));
+  li.appendChild(btn);
+  listItems.appendChild(li);
+}
+//add Items to Local Storage
+function addItemsToStorage(item) {
+  const itemsFromStorage = getItemFromStorage();
+  itemsFromStorage.push(item);
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+//display the acctual local storage items to the dom
+function displayItem() {
+  const itemsFromStorage = getItemFromStorage();
+  itemsFromStorage.forEach((item) => {
+    addItemsToDOM(item);
+  });
+  clearAllHide();
+}
+//only the function which get the item from the storage
+function getItemFromStorage() {
+  let itemsFromStorage;
+  if (localStorage.getItem("items") === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+  }
+  return itemsFromStorage;
+}
+function OnClickItem(e) {
   if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove();
-    clearAllHide();
+    removeItem(e.target.parentElement);
   }
 }
+//remove item from list
+function removeItem(item) {
+  item.remove();
+  //removeing acctual item from storage
+  removeItemFromStorage(item.firstChild.textContent);
+  clearAllHide();
+}
+//remove item from storage
+function removeItemFromStorage(item) {
+  let itemsFromStorage = getItemFromStorage();
+  console.log(itemsFromStorage);
+  console.log(item);
+  itemsFromStorage = itemsFromStorage.filter((i) => i !== item);
+  localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+
+//clear all the items from the list
 function clearAll() {
   listItems.innerHTML = "";
   clearAllHide();
 }
+//if there no any item clearAll button will be desabled
 function clearAllHide() {
   const li = document.querySelectorAll("li");
   if (li.length === 0) {
@@ -52,6 +97,7 @@ function clearAllHide() {
     filter.style.display = "block";
   }
 }
+//filter search function from the list items
 function filterAll(e) {
   const value = e.target.value.toLowerCase();
   const li = document.querySelectorAll("li");
@@ -62,28 +108,12 @@ function filterAll(e) {
     } else {
       item.style.display = "none";
     }
-
-    //do from here
   });
 }
 
 inputForm.addEventListener("submit", addItem);
-listItems.addEventListener("click", removeItem);
+listItems.addEventListener("click", OnClickItem);
 clear.addEventListener("click", clearAll);
 filterInput.addEventListener("input", filterAll);
 clearAllHide();
-
-// submitBtn.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log("clicked");
-//   another.innerHTML += `<div class="list-items">
-//             <div class="items">${foundValue()}</div>
-//             <button class="btn">Delete</button>
-//           </div>`;
-
-// });
-// InputBox.addEventListener("keypress", foundValue);
-
-// function foundValue() {
-//   return InputBox.value;
-// }
+document.addEventListener("DOMContentLoaded", displayItem);
